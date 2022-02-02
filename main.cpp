e937d40492046d9dd76672c2f1addbc1ef21e467
lionel@lionel-pc2 <lionel.desorme@free.fr>
2022-02-02 19:09:03 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -6,17 +6,25 @@ template<typename T>
 void check_align()
 {
 	
-	unsigned long long mask = (1<<8)-1;
+	unsigned long long mask = ~0;
 	std::vector<std::vector<T>> tab;
 	for (int i = 0; i < 1000; i++)
 	{
 		std::vector<T> v(1);
+		v[0] = T();
 		unsigned long long p = (unsigned long long)(&v[0]);
 		mask = mask & (~p);
 		//std::cout << v.size() << " " << &v[0] << std::endl;
 		tab.push_back(v);
 	}
-	std::cout << typeid(T).name() << ": " << (mask + 1) * 8 << std::endl;
+	unsigned long long align = 1;
+	while (mask & 1)
+	{
+		align *= 2;
+		mask = mask >> 1;
+	}
+
+	std::cout << typeid(T).name() << ": " << align << " ("<< align *8 << ")"<<  std::endl;
 }
 
 int main(int argc, char** argv)
